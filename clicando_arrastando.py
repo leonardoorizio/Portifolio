8e294302b385cad5ee7d68082f271a37272d6980
Leonardo Orizio <amaroki.633@gmail.com>
2025-01-06 19:56:52 -0300

    v1

---
+++ clicando_arrastando.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+for i in range(10):
+    pyautogui.moveTo(1237,764, duration = 0.2)
+    pyautogui.dragTo(1304,294, button = 'left',duration = 0.2)
---
+++ desafio_click.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+pyautogui.moveTo(1342,487, duration = 0.2)
+pyautogui.click(button='right')
+pyautogui.moveTo(1398,642, duration = 0.2)
+pyautogui.click()
+pyautogui.moveTo(1077,199, duration = 0.2)
+pyautogui.click()
