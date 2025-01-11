b0e722684a31dfd75059df4fff7fabe7f469ad30
Leonardo Orizio <amaroki.633@gmail.com>
2025-01-11 17:24:56 -0300

    v1

--- desafio_escrever.py
+++ desafio_escrever.py
@@ -10,3 +10,5 @@ def escrever_frase(frase):
 pyautogui.moveTo(1397, 402, duration=0.2)
 pyautogui.click()
 escrever_frase('Automação é incrivel!!!')
+
+# Caso não contenha caracteres especiais, podemos utilizar a função typewrite
